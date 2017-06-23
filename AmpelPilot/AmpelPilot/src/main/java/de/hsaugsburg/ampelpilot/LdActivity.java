85da9021d3d31993c7ea17f2b5d2b18f91d2d841
Quirin Büchner <quirin.buechner@hs-augsburg.de>
2017-06-23 20:37:07 +0200

    improved permission request for camera

--- AmpelPilot/AmpelPilot/src/main/java/de/hsaugsburg/ampelpilot/LdActivity.java
+++ AmpelPilot/AmpelPilot/src/main/java/de/hsaugsburg/ampelpilot/LdActivity.java
@@ -2,15 +2,22 @@ package de.hsaugsburg.ampelpilot;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -32,11 +39,13 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+
 import android.Manifest;
 import android.support.v4.app.ActivityCompat;
 
 
 import android.os.Vibrator;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -46,30 +55,29 @@ import java.util.Locale;
 
 import static android.R.attr.button;
 
-public class LdActivity extends Activity implements CvCameraViewListener2, SensorEventListener{
+public class LdActivity extends Activity implements CvCameraViewListener2, SensorEventListener {
 
-    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
-    private static final String    TAG                 = "OCVSample::Activity";
+    private static final String TAG = "OCVSample::Activity";
     private static final Scalar GREEN_RECT_COLOR = new Scalar(0, 255, 0, 255);
-    private static final Scalar    RED_RECT_COLOR     = new Scalar(255, 0, 0, 255);
-    public static final int        JAVA_DETECTOR       = 0;
+    private static final Scalar RED_RECT_COLOR = new Scalar(255, 0, 0, 255);
+    public static final int JAVA_DETECTOR = 0;
     private SensorManager mSensorManager;
     private Sensor mSensor;
 
-    private  LightPeriod lightgreen = new LightPeriod();
-    private  LightPeriod lightred = new LightPeriod();
+    private LightPeriod lightgreen = new LightPeriod();
+    private LightPeriod lightred = new LightPeriod();
     long SytsemTime = System.currentTimeMillis();
     private TextToSpeech tts;
     //status check code
     private int MY_DATA_CHECK_CODE = 0;
     int method = 0;
 
-    private Mat                    mRgba;
-    private Mat                    mGray;
-    private Mat                    mRgbaT;
-    private Mat                    mRgbaF;
-    private File                   mCascadeFileGreen;
-    private File                   mCascadeFileRed;
+    private Mat mRgba;
+    private Mat mGray;
+    private Mat mRgbaT;
+    private Mat mRgbaF;
+    private File mCascadeFileGreen;
+    private File mCascadeFileRed;
     private CascadeClassifier mJavaDetectorGreen;
     private CascadeClassifier mJavaDetectorRed;
 
@@ -86,21 +94,21 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
     private TextView y;
     private TextView z;
 
-    private double                  scaleFactor;
-    private int                     minNeighbours;
+    private double scaleFactor;
+    private int minNeighbours;
 
-    private int                      Zoom;
+    private int Zoom;
 
-    private double                  scaleFactorRED;
-    private int                     minNeighboursRED;
+    private double scaleFactorRED;
+    private int minNeighboursRED;
 
 
-    private int                    mDetectorType       = JAVA_DETECTOR;
-    private String[]               mDetectorName;
+    private int mDetectorType = JAVA_DETECTOR;
+    private String[] mDetectorName;
 
     private int mTrafficLightSize = 0;
 
-    private CameraBridgeViewBase   mOpenCvCameraView;
+    private CameraBridgeViewBase mOpenCvCameraView;
     private TextView mValue;
 
     long millis = System.currentTimeMillis();
@@ -108,13 +116,12 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
     double xCenter = -1;
     double yCenter = -1;
 
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
 
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
                     try {
                         // load cascade file from application resources
@@ -169,11 +176,12 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
                     mOpenCvCameraView.setClickable(false);
 
                     mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
@@ -185,15 +193,15 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
-    /** Called when the activity is first created. */
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
 
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.CAMERA},
-                MY_PERMISSIONS_REQUEST_CAMERA);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
@@ -213,7 +221,7 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
         });
         setContentView(R.layout.trafficlights_detect_surface_view);
 
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
@@ -226,20 +234,20 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
         z = (TextView) findViewById(R.id.z);
 
         Intent i = getIntent();
-        minNeighbours = i.getIntExtra("MinNeighbours",15);
-        scaleFactor = i.getDoubleExtra("ScaleFactor",15);
+        minNeighbours = i.getIntExtra("MinNeighbours", 15);
+        scaleFactor = i.getDoubleExtra("ScaleFactor", 15);
 
-        minNeighboursRED = i.getIntExtra("MinNeighboursRED",15);
-        scaleFactorRED = i.getDoubleExtra("ScaleFactorRED",15);
+        minNeighboursRED = i.getIntExtra("MinNeighboursRED", 15);
+        scaleFactorRED = i.getDoubleExtra("ScaleFactorRED", 15);
 
-        Zoom = i.getIntExtra("ZoomFactor",10);
+        Zoom = i.getIntExtra("ZoomFactor", 10);
 
         v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
         // Vibrate for 500 milliseconds
         v.vibrate(500);
 
-        btnSettings.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
                 finish();
             }
         });
@@ -248,8 +256,7 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
         mSensorManager.unregisterListener(this);
@@ -258,13 +265,11 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
     }
 
 
-
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
-       mSensorManager.registerListener(this, accelerometer ,SensorManager.SENSOR_DELAY_UI);
-       mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
@@ -317,48 +322,45 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
         MatOfRect red = new MatOfRect();
 
         if (mDetectorType == JAVA_DETECTOR) {
-            if (mJavaDetectorGreen != null){
+            if (mJavaDetectorGreen != null) {
                 mJavaDetectorGreen.detectMultiScale(mRgba, green, scaleFactor, minNeighbours, 0, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
-                        new Size(20, 40), new Size(200,400));
+                        new Size(20, 40), new Size(200, 400));
             }
-            if(mJavaDetectorRed!= null){
+            if (mJavaDetectorRed != null) {
                 mJavaDetectorRed.detectMultiScale(mRgba, red, scaleFactorRED, minNeighboursRED, 0,
-                        new Size(20, 40), new Size(200,400));
+                        new Size(20, 40), new Size(200, 400));
             }
-        }
-        else {
+        } else {
             Log.e(TAG, "Detection method is not selected!");
         }
 
         Rect[] greenArray = green.toArray();
         lightgreen.addpoint(greenArray);
-        if(lightgreen.checklight()){
-            Log.w("step","onCameraFrame: "+ System.currentTimeMillis() +"   " +SytsemTime);
-            if((System.currentTimeMillis() - SytsemTime )>2000){
+        if (lightgreen.checklight()) {
+            Log.w("step", "onCameraFrame: " + System.currentTimeMillis() + "   " + SytsemTime);
+            if ((System.currentTimeMillis() - SytsemTime) > 2000) {
                 speak("Es ist Grün");
-                SytsemTime=System.currentTimeMillis();
+                SytsemTime = System.currentTimeMillis();
             }
 
 
-            Log.w("step","onCameraFrame:  #################### Grün wurde erkannt bÄÄÄÄÄm");
+            Log.w("step", "onCameraFrame:  #################### Grün wurde erkannt bÄÄÄÄÄm");
         }
-        for (int i = 0; i < greenArray.length; i++)
-        {
+        for (int i = 0; i < greenArray.length; i++) {
             Imgproc.rectangle(mRgba, greenArray[i].tl(), greenArray[i].br(),
                     GREEN_RECT_COLOR, 3);
         }
 
         Rect[] redArray = red.toArray();
         lightred.addpoint(redArray);
-        if(lightred.checklight()){
-            if(( System.currentTimeMillis()- SytsemTime )>2000){
+        if (lightred.checklight()) {
+            if ((System.currentTimeMillis() - SytsemTime) > 2000) {
                 speak("Es ist Rot");
-                SytsemTime=System.currentTimeMillis();
+                SytsemTime = System.currentTimeMillis();
             }
-            Log.w("step","onCameraFrame:  #################### Red wurde erkannt bÄÄÄÄÄm");
+            Log.w("step", "onCameraFrame:  #################### Red wurde erkannt bÄÄÄÄÄm");
         }
-        for (int i = 0; i < redArray.length; i++)
-        {
+        for (int i = 0; i < redArray.length; i++) {
             Imgproc.rectangle(mRgba, redArray[i].tl(), redArray[i].br(),
                     RED_RECT_COLOR, 3);
         }
@@ -367,17 +369,17 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
     }
 
 
-    private void speak(String text){
+    private void speak(String text) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-        }else{
+        } else {
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
         }
     }
 
     public void onSensorChanged(SensorEvent event) {
 
-        long newMillis = System.currentTimeMillis() ;
+        long newMillis = System.currentTimeMillis();
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
             mGravity = event.values;
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
@@ -406,57 +408,56 @@ public class LdActivity extends Activity implements CvCameraViewListener2, Senso
                 //roll kleiner 1.4
                 double valueRoll = 1.4;
                 if ((abs(roll) <= valueRoll) && (newMillis > millis + 1500)) {
-                    float t = ( 150/ (abs(roll)) -20);
+                    float t = (150 / (abs(roll)) - 20);
                     v.vibrate((long) (t));
                     millis = newMillis;
-                    if((System.currentTimeMillis() - SytsemTime )>5000){
+                    if ((System.currentTimeMillis() - SytsemTime) > 5000) {
                         speak("Winkel zu Niedrig");
-                        SytsemTime=System.currentTimeMillis();
+                        SytsemTime = System.currentTimeMillis();
                     }
                 }
 
                 //roll größer 2
                 if ((abs(roll) >= valueRoll + diffRoll) && (newMillis > millis + 1500)) {
-                    float t = ( (abs(roll) *100) -50);
+                    float t = ((abs(roll) * 100) - 50);
                     v.vibrate((long) (t));
                     millis = newMillis;
-                    if((System.currentTimeMillis() - SytsemTime )>5000){
+                    if ((System.currentTimeMillis() - SytsemTime) > 5000) {
                         speak("Winkel zu Hoch");
-                        SytsemTime=System.currentTimeMillis();
+                        SytsemTime = System.currentTimeMillis();
                     }
                 }
-                double valueazimut =0;
 
                 //pitch größer 0,2
                 double valuePitch = 0;
-                if ((( pitch <= valuePitch - diffPitch)) && (newMillis > millis + 1500)) {
+                if (((pitch <= valuePitch - diffPitch)) && (newMillis > millis + 1500)) {
                     float t = ((abs(pitch) * 1000) - 50);
                     v.vibrate((long) (t));
                     millis = newMillis;
-                    if((System.currentTimeMillis() - SytsemTime )>5000){
+                    if ((System.currentTimeMillis() - SytsemTime) > 5000) {
                         speak("Zu weit nach rechts geneigt");
-                        SytsemTime=System.currentTimeMillis();
+                        SytsemTime = System.currentTimeMillis();
                     }
                 }
                 if (((pitch >= valuePitch + diffPitch)) && (newMillis > millis + 1500)) {
                     float t = ((abs(pitch) * 1000) - 50);
                     v.vibrate((long) (t));
                     millis = newMillis;
-                    if((System.currentTimeMillis() - SytsemTime )>5000){
+                    if ((System.currentTimeMillis() - SytsemTime) > 5000) {
                         speak("Zu weit nach links geneigt");
-                        SytsemTime=System.currentTimeMillis();
+                        SytsemTime = System.currentTimeMillis();
                     }
                 }
 
                 // Azimut größer 2
                 double valueAzimut = 2.3;
                 if ((abs(azimut) >= valueAzimut) && (newMillis > millis + 1500)) {
-                    float t = ( 500);
+                    float t = (500);
                     v.vibrate((long) (t));
                     millis = newMillis;
-                    if((System.currentTimeMillis() - SytsemTime )>5000){
+                    if ((System.currentTimeMillis() - SytsemTime) > 5000) {
                         speak("Drehen Sie das Handy");
-                        SytsemTime=System.currentTimeMillis();
+                        SytsemTime = System.currentTimeMillis();
                     }
                 }
             }
---
+++ AmpelPilot/AmpelPilot/src/main/java/de/hsaugsburg/ampelpilot/SplashScreen.java
@@ -0,0 +1,106 @@
+package de.hsaugsburg.ampelpilot;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+
+
+public class SplashScreen extends Activity {
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    private boolean cameraPermission;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (permissions.length > 0)
+            switch (permissions[0]) {
+                case Manifest.permission.CAMERA:
+                    if (cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        // permission granted, onResume will start next activity
+
+                    } else {
+                        new Handler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                showCameraRequestDialog();
+                            }
+                        });
+                        break;
+                    }
+            }
+    }
+
+    private void showCameraRequestDialog() {
+        AlertDialog dlg = new AlertDialog.Builder(this)
+                .setTitle("Kein Kamerazugriff")
+                .setMessage("Ampelpilot benötigt Zugriff auf die Kamera.")
+                .setPositiveButton("Zugriff anfordern.", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // delay so that the alert-dialog has time to close
+                        new Handler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                ActivityCompat.requestPermissions(SplashScreen.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                            }
+                        });
+                    }
+                })
+                .setNeutralButton("App-Einstellungen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final Intent i = new Intent();
+                        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        i.addCategory(Intent.CATEGORY_DEFAULT);
+                        i.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                        startActivity(i);
+                    }
+                })
+                .create();
+        dlg.show();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+
+        if (cameraPermission) {
+            startCameraActivity();
+        }
+    }
+
+    void startCameraActivity() {
+        Intent i = new Intent(this, LdActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
